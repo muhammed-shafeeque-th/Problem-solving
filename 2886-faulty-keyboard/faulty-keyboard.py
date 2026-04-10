cea8dd31b34ec37defb08a5be13cfe887103519a
Muhammed Shafeeque th <shafeeqsha06@gmail.com>
2026-04-10 10:50:20 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (31.11%) - LeetSync

---
+++ 2886-faulty-keyboard/faulty-keyboard.py
@@ -0,0 +1,10 @@
+class Solution:
+    def finalString(self, s: str) -> str:
+        result = []
+        for ch in s:
+            if ch == "i":
+                result.reverse()
+            else:
+                result.append(ch)
+        return "".join(result)
+        
